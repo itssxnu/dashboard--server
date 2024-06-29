3edbf68fcf9454da779fb3f1930b02d04b9195d7
Kavindu Aluthwaththa <162307625+KavinduAluthwaththa@users.noreply.github.com>
2024-06-29 15:08:37 +0530

    file update

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, render_template
 import paho.mqtt.client as mqtt
 import json
 import threading
@@ -9,28 +9,40 @@ MQTT_BROKER = 'localhost'
 MQTT_PORT = 1883
 MQTT_TOPIC = 'test'  # Replace with your topic
 
+mqtt_data={
+
+    'suitable_slot': 'N/A',
+    'slot_1': 'N/A',
+    'slot_2': 'N/A',
+    'slot_3': 'N/A',
+    'vehicle_size': 'N/A'
+}
+
 @app.route('/')
 def index():
-    return "Pake MQTT Flask App"
+    return render_template("index.html", **mqtt_data)
 
 def on_connect(client, userdata, flags, rc):
     print(f"Connected with result code {rc}")
     client.subscribe(MQTT_TOPIC)
 
 def on_message(client, userdata, msg):
+    global mqtt_data
     payload = msg.payload.decode()
     print(f"Received message: {payload}")
     data = json.loads(payload)
-    vehicle_size = data.get('size')
-    print(f"Vehicle Size: {vehicle_size}")
     
-    # Print slot based on vehicle size
-    if vehicle_size == 1:
-        print("slot_1")
-    elif vehicle_size == 2:
-        print("slot_2")
-    else:
-        print("slot_3")
+    with threading.Lock():
+        mqtt_data['vehicle_size'] = data.get('size', 'N/A')
+        if mqtt_data['vehicle_size'] == 1:
+            mqtt_data['suitable_slot'] = 'Slot 1'
+            mqtt_data['slot_1'] = 'Occupied'
+        elif mqtt_data['vehicle_size'] == 2:
+            mqtt_data['suitable_slot'] = 'Slot 2'
+            mqtt_data['slot_2'] = 'Available'
+        else:
+            mqtt_data['suitable_slot'] = 'Slot 3'
+            mqtt_data['slot_3'] = 'Available'
 
 mqtt_client = mqtt.Client()
 mqtt_client.on_connect = on_connect
